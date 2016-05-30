e86e62d493698a7c6acfad445cb1c1a5fbd0759b
Chirag <chirag-parmar@live.com>
2016-05-30 11:22:31 -0300

    eclipse dependency changes

--- src/main/java/analytics/AnalyticsEngine.java
+++ src/main/java/analytics/AnalyticsEngine.java
@@ -4,7 +4,6 @@ import interfaces.IAnalyticsEngine;
 
 public class AnalyticsEngine implements IAnalyticsEngine {
 
-	@Override
 	public boolean IsReady() {
 		// TODO Auto-generated method stub
 		return false;

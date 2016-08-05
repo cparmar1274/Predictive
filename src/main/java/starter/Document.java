2c965fb56b3dcf32b251a3a5dd60de88d5ea0d84
cparmar1274 <chirag-parmar@live.com>
2016-08-05 16:40:56 -0300

    document changes

--- src/main/java/starter/Document.java
+++ src/main/java/starter/Document.java
@@ -3,18 +3,22 @@ package starter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-class Node {
-	
+import com.google.gson.Gson;
+
+class Node{
+
 	public Heading heading;
 	public List<Node> children;
+	public boolean recent;
+	
+	public Node() {
+		super();
+		this.children = new ArrayList<Node>();
+	}
 
 	public Heading getHeading() {
 		return heading;
@@ -31,11 +35,23 @@ class Node {
 	public void addChildren(Node child) {
 		this.children.add(child);
 	}
+	
+	public void setRecent(boolean recent){
+		this.recent = recent;
+	}
+	
+	public boolean isRecent(){
+		return this.recent;
+	}
 
 	@Override
 	public String toString() {
-		return "Node [heading=" + heading + ", children=" + children + "]";
+		return "Node [heading=" + heading + ", children=" + children
+				+ ", recent=" + recent + "]";
 	}
+
+	
+
 
 }
 
@@ -43,179 +59,186 @@ class Heading {
 
 	public String text;
 	public Integer weight;
-	public String type;
-	public String startsWith;
-
+	    
 	public Heading(String type, String value) {
 		super();
-		this.type = type;
 		this.text = value;
-		this.weight = 1;
-		this.startsWith = IndexUtility.getStartsWith(type);
+		this.weight = IndexUtility.getWeight(type);
 	}
-
-	public Object getCurrentIndex() {
-
-		Object currentIndex;
-
-		switch (this.startsWith) {
-		case IndexUtility.ALPHABET_KEY:
-			currentIndex = IndexUtility.ALPHABET_VALUES.get(this.weight);
-			break;
-		case IndexUtility.ROMAN_KEY:
-			currentIndex = IndexUtility.ROMAN_VALUES.get(this.weight);
-			break;
-		default:
-			currentIndex = this.weight;
-			break;
-		}
-
-		return currentIndex;
+	
+	@Override
+	public String toString() {
+		return "[ " + this.weight + " - " + this.text + " ]";
 	}
 
 	@Override
-	public String toString() {
-		return "[ " + this.type + " - " + this.text + " - " + this.weight
-				+ " - " + this.startsWith + " - " + this.getCurrentIndex()
-				+ " ]";
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		result = prime * result + ((weight == null) ? 0 : weight.hashCode());
+		return result;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Heading other = (Heading) obj;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		if (weight == null) {
+			if (other.weight != null)
+				return false;
+		} else if (!weight.equals(other.weight))
+			return false;
+		return true;
+	}
+
+	
 }
 
 class DocumentParserEngine {
 
 	public String filePath;
+	public Node node;
 
 	public DocumentParserEngine(String filePath) {
 		this.filePath = filePath;
+		this.node = new Node();
 	}
 
-	
-	
-	public List<Heading> generateParsedDocument() throws IOException {
+	public void generateParsedDocument() throws IOException {
 
-		Path path = Paths.get(this.filePath);
-		List<String> lines = Files.readAllLines(path, Charset.defaultCharset());
+		Node currentNode;
+		List<String> lines = this.readDocument();
 
-		Map<Integer, Integer> weightedDocs = new HashMap<Integer, Integer>();
-		List<Heading> readyToDisplay = new ArrayList<Heading>();
-
-		Integer preKey = null;
-		Node node = new Node();	
 		for (String line : lines) {
 
 			if (line.isEmpty() || !line.toUpperCase().startsWith("HEADING"))
 				continue;
 
 			String[] lineSplit = line.split("\\t");
-			Integer key = IndexUtility.getWeight(lineSplit[0].trim());
-			String value = lineSplit[1].trim();
+			Heading displayObject = new Heading(lineSplit[0].trim(),lineSplit[1].trim());
+			currentNode = new Node();
+			currentNode.heading = displayObject;
+			currentNode.setRecent(true);
 			
-			if (weightedDocs.containsKey(key)) {
-				Integer docIndex = weightedDocs.get(key);
-				docIndex++;
-				if (preKey < key)
-					weightedDocs.put(key, 1);
-				else
-					weightedDocs.put(key, docIndex);
+			//if root node doesn't have child, add first node as child
+			if(this.node.getChildren().isEmpty()) {
+				this.node.addChildren(currentNode);
 			} else {
-				weightedDocs.put(key, 1);
-			}
-
-			Heading displayObject = new Heading(lineSplit[0].trim(), value);
-			displayObject.weight = weightedDocs.get(key);
-			readyToDisplay.add(displayObject);
-			addHeadingToTree(node,displayObject);
-			preKey = key;
-		}
-		System.out.println("Node : "+node);
-		return readyToDisplay;
-	}
-
-	private void addHeadingToTree(Node node,Heading displayObject) {
-		if(node.heading==null){
-			node.heading = displayObject;
-		}
-		if(node.children==null){
-			node.children = new ArrayList<Node>();
-		}
-		
-		if(displayObject.weight-node.heading.weight == 1){
-			Node nNode = new Node();
-			nNode.heading = displayObject;
-			node.addChildren(nNode);
-		} else {
-			for(Node cNode : node.getChildren()){
-				addHeadingToTree(cNode, displayObject);
+				//find out where to add next node 
+				Node latestNode = this.getLatestNode(this.node);
+				
+				//latestnode is null as there is a change in weight, find node where its weight is one weight low from latest node
+				if(latestNode==null){
+					latestNode = this.findNode(this.node,currentNode);
+				}
+				
+				latestNode.setRecent(false);
+				if(currentNode.heading.weight > latestNode.heading.weight){
+					this.addNode(latestNode,currentNode);
+				} else if (currentNode.heading.weight == latestNode.heading.weight) {
+					//find parent of latest node and add current node into it
+					Node parentNode = this.getParentNode(latestNode,this.node);
+					parentNode.addChildren(currentNode);
+					currentNode.setRecent(false);
+				} 
 			}
 		}
-		
 	}
 
+	private Node getParentNode(Node latestNode,Node parentNode) {
+		for(Node nodeChild: parentNode.getChildren()){
+			if(nodeChild.heading.equals(latestNode.heading))
+				return parentNode;
+			else
+				return getParentNode(latestNode,nodeChild);
+		}
+		return null;
+	}
+	
+	private Node findNode(Node master,Node currentNode) {
+			
+		for(Node nodeChild: master.getChildren()){
+			if(nodeChild.heading.weight==currentNode.heading.weight)
+				return master;
+			else
+				return findNode(nodeChild,currentNode);
+		}
+		return null;
+	}
+
+	private Node getLatestNode(Node node) {
+		
+		for(Node nodeChild: node.getChildren()){
+			if(nodeChild.isRecent())
+				return nodeChild;
+			else
+				return getLatestNode(nodeChild);
+		}
+		return null;
+	}
+
+	private boolean addNode(Node parent, Node childNode) {
+		
+		//find out where to add next node
+		int totalChild = parent.getChildren().size();
+		if(totalChild == 0){
+			parent.addChildren(childNode);
+			return true;
+		}
+		Node latestNode = parent.getChildren().get(totalChild-1);
+		int latestNodeWeight = childNode.heading.weight;
+		
+		if(parent.heading.weight > latestNodeWeight){
+			this.addNode(latestNode,childNode);
+		} else{ 
+			parent.addChildren(latestNode);
+		}
+		return true;
+	}
+
+	public void getGeneratedTree() {
+		System.out.print(new Gson().toJson(this.node));
+	}
+
+	public List<String> readDocument() throws IOException {
+		return Files.readAllLines(Paths.get(this.filePath), Charset.defaultCharset());
+	}
+
+}
+
+class IndexUtility {
+	public static Integer getWeight(String type) {
+		if (!type.isEmpty() && type.toUpperCase().contains("HEADING"))
+			return Integer.parseInt(type.toUpperCase().replaceAll("HEADING", "").trim());
+		
+		return 1;
+	}
 }
 
 public class Document {
 
 	public static void main(String[] args) {
 		try {
-			Long startTime = System.currentTimeMillis();
+			
 			String filePath = "//home//chiragparmar//workspace//Predictive//src//main//java//starter//document//document.txt";
 			DocumentParserEngine docEngine = new DocumentParserEngine(filePath);
-			List<Heading> parsedDocument = docEngine.generateParsedDocument();
-			System.out.println("Parsing took : "
-					+ (System.currentTimeMillis() - startTime) + " ms");
-			for (Heading docObj : parsedDocument) {
-				System.out.println(IndexUtility.getTabbing(docObj.type)
-						+ docObj.getCurrentIndex() + ". " + docObj.text);
-			}
-			System.out.println(parsedDocument);
+			docEngine.generateParsedDocument();
+			docEngine.getGeneratedTree();
+			
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.print(e.getMessage());
 		}
 	}
 
-}
-
-class IndexUtility {
-
-	public static final String ALPHABET_KEY = "ALPHABET";
-	public static final List<String> ALPHABET_VALUES = Arrays
-			.asList("#,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,,R,S,T,U,V,W,X,Y,Z"
-					.split(","));
-
-	public static final String ROMAN_KEY = "ROMAN";
-	public static final List<String> ROMAN_VALUES = Arrays
-			.asList("#,I,II,III,IV,V,VI,VII,VIII,IX,X".split(","));
-
-	public static final String NUMERIC_KEY = "NUMERIC";
-
-	public static String getStartsWith(String type) {
-
-		if (!type.isEmpty() && type.trim().equalsIgnoreCase("HEADING 2"))
-			return ALPHABET_KEY;
-
-		if (!type.isEmpty() && type.trim().equalsIgnoreCase("HEADING 3"))
-			return ROMAN_KEY;
-
-		return NUMERIC_KEY;
-	}
-
-	public static Integer getWeight(String type) {
-		if (!type.isEmpty() && type.toUpperCase().contains("HEADING"))
-			return Integer.parseInt(type.toUpperCase()
-					.replaceAll("HEADING", "").trim());
-
-		return 1;
-	}
-
-	public static String getTabbing(String type) {
-
-		if (!type.isEmpty() && type.trim().equalsIgnoreCase("HEADING 2"))
-			return "\t";
-
-		if (!type.isEmpty() && type.trim().equalsIgnoreCase("HEADING 3"))
-			return "\t\t";
-
-		return "";
-	}
 }

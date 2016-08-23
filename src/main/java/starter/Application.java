00ade25c8ef282c4a9efa38e6e0e0b83c21886cf
cparmar1274 <chirag-parmar@live.com>
2016-08-23 16:26:47 -0300

    changes

--- src/main/java/starter/Application.java
+++ src/main/java/starter/Application.java
@@ -1,30 +1,16 @@
 package starter;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableWebMvc
 @ComponentScan(basePackages = { "starter", "analytics", "authentications" })
 public class Application {
 
-	public static EntityManagerFactory EntityManagerFactory = null;
-	public static EntityManager EManager = null;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
-		EntityManagerFactory = Persistence.createEntityManagerFactory("mammoth");
-		EManager = EntityManagerFactory.createEntityManager();
-
 	}
 
 }
